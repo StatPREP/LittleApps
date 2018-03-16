b74a4bcf143257f4e59c92d580a7dec1110c8a2c
Danny Kaplan <kaplan@macalester.edu>
2018-03-16 17:57:38 -0500

    finishing littleapps-proportions

--- R/reactive.R
+++ R/reactive.R
@@ -6,8 +6,9 @@
 #' in reactive functions are often produced by other reactive functions.
 #'
 #' @param stmts bracketed code of the same sort used in `shiny::reactive()`
-#' @param shiny flag indicated whether a shiny-reactive function or a mock-reactive
-#' function should be produced.
+#' @param env variable to set the environment in which the statements will be executed.
+#' It would be rare to use the `env` variable: the default does what's needed
+#' @param ... a single object (of whatever kind) to be stored in a mock-reactive variable
 #'
 #' @examples
 #' input <- list(samp_n = 15)
@@ -17,17 +18,35 @@
 #'
 #' find_rsquared()  # the ultimate calculation
 #'
-#'
+#' v <- reactive_value_holder(7)
+#' v() # get value
+#' v(88) # set value (and return that value)
+#' v() # get value
 #' @export
-react_calc <- function(stmts, shiny=FALSE, env = parent.frame()) {
+react_calc <- function(stmts, env = parent.frame()) {
   stmts <- substitute(stmts)
-  if (shiny) {
-    F <- reactive(stmts, env = env)
-  } else {
-    F <- function() { NULL }
-    body(F) <- stmts
-  }
+
+  F <- function() { NULL }
+  body(F) <- stmts
+
   F
+}
+
+#' A mock-reactive equivalent to reactiveVal
+
+#' @export
+reactive_value_holder <- function(...) {
+  inputs <- list(...)
+  state <- if (length(inputs) == 1) inputs[[1]]
+  else NULL
+  f <- function(...) {
+    inputs <- list(...)
+    if (length(inputs) == 1)
+      state <<- inputs[[1]]
+
+    state
+  }
+
 }
 
 
