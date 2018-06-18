6c8e397f265e29fa2b8e4d5f704777b49b9ded9a
Randall Pruim <rpruim@gmail.com>
2018-06-18 14:11:57 -0400

    add spaces

--- R/LittleApps.R
+++ R/LittleApps.R
@@ -2,11 +2,11 @@
 #'
 #' In order to include Shiny apps in a learnr tutorial, the apps
 #' have been written as Rmd documents. These can then be included in a tutorial
-#' by using the `child=system.file("name_of_app.Rmd", package = "LittleApps")` option in
-#' a chunk.
+#' by using the `child = system.file("name_of_app.Rmd", package = "LittleApps")`
+#' option in a chunk.
 #'
 #' @details In the app Rmd files, the chunks containing the server part of
-#' the app should be marked as `context="server"`.
+#' the app should be marked as `context = "server"`.
 #'
 #' @name LittleApps
 NULL
